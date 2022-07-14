fa03643c07beab21aa5348e95c7465c3f45fb17d
Juan Silva <juan.silvawd@gmail.com>
2022-07-14 11:13:39 -0500

    jstl exer

---
+++ src/main/java/login.java
@@ -0,0 +1,56 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class login {
+    @WebServlet("/Login")
+    public class Login extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * @see HttpServlet#HttpServlet()
+         */
+        public Login() {
+            super();
+            // TODO Auto-generated constructor stub
+        }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+        {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            //user credentials are verified
+            //if credentials are correct,it creates session object
+            //and takes to welcome.jsp
+            if(username.equals("admin") && password.equals("1234"))
+            {
+                //setting userdata into a session object
+                HttpSession session = request.getSession();
+                session.setAttribute("username",username);
+                response.sendRedirect("welcome.jsp");
+            }
+            //otherwise it will takes to login.jsp
+            else
+            {
+                response.sendRedirect("login.jsp");
+            }
+        }
+
+        /**
+         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            // TODO Auto-generated method stub
+            doGet(request, response);
+        }
+
+    }
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: juansfolder
+  Date: 7/14/22
+  Time: 10:12 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+         pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>login</title>
+</head>
+<body>
+<!--login.java is invoked when login button is pressed-->
+<form action = "Login" method="post">
+    Enter username: <input type="text" name="username"><br>
+    Enter password: <input type="password" name="password"><br>
+    <input type="submit" value="login">
+</form>
+</body>
+</html>
