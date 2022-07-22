c5b562bc0ea2fe50eb3698feefd4d81de9e08379
Juan Silva <juan.silvawd@gmail.com>
2022-07-22 15:08:22 -0500

    hello ex

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,18 @@
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        if (name == null) {
+            name = "World";
+        } else if (name.equalsIgnoreCase("jeff-bezos")) {
+            response.sendRedirect("https://amazon.com");
+            return;
+        }
+        request.setAttribute("name", name);
+        request.getRequestDispatcher("/WEB-INF/hello-world.jsp").forward(request, response);
     }
 }
